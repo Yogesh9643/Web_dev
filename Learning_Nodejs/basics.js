db2c072a24567eba690a5b3a5f02bb13cd789d3d
Yogesh9643 <rajputyogesh563@gmail.com>
2020-05-12 15:21:21 +0530

    expressjs basics

--- Learning_Nodejs/basics.js
+++ Learning_Nodejs/basics.js
@@ -6,7 +6,7 @@ const app= express();
 app.use(bodyParser.urlencoded());
 
 app.use('/add-product',(req, res, next)=>{
-app.send('<form action = "/product"  method ="POST"><input type="text" name= "title"><button type="submit"> Add Product</button> </form');
+res.send('<form action = "/product"  method ="POST"><input type="text" name= "title"><button type="submit"> Add Product</button> </form');
 });
 
 app.use('/product' , (req, res, next)=> {
