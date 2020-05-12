eb6f6a68bbc5c53c5d2a904d146031258dab10e7
Yogesh9643 <rajputyogesh563@gmail.com>
2020-05-12 15:16:15 +0530

    Using Expressjs basics

--- Learning_Nodejs/basics.js
+++ Learning_Nodejs/basics.js
@@ -1,8 +1,22 @@
-const http=require('http');
 
-const server=http.createServer(function(req,res){
-    console.log(req.url, req.method, req.headers );
-    process.exit();
+const express=require('express');
+cons bodyParser= require('body-parser');
+
+const app= express();
+app.use(bodyParser);
+
+app.use('/add-product',(req, res, next)=>{
+app.send('<form action = "/product"  method ="POST"><input type="text" name= "title"><button type="submit"> Add Product</button> </form');
+});
+
+app.use('/product' , (req, res, next)=> {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from express js</h1>');
 })
 
-server.listen(3000);
+
+app.listen(3000);  
