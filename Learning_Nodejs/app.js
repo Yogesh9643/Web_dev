582082a97dcd93c6a213fdd62a060263ef2ec0b5
Yogesh9643 <rajputyogesh563@gmail.com>
2020-05-12 15:44:16 +0530

    adding router

---
+++ Learning_Nodejs/app.js
@@ -0,0 +1,17 @@
+
+const express= require('express');
+const bodyParser= require('body-parser');
+
+const app= express();
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+const adminRouter= require ( './routes/admin')
+const shopRouter = require('./routes/shop')
+
+app.use(adminRouter);
+app.use(shopRouter);
+
+
+
+app.listen(3000);  
