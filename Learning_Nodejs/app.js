f483715454fa995035b85ddaa66f839593b6d169
Yogesh9643 <rajputyogesh563@gmail.com>
2020-05-14 19:36:03 +0530

    styling and routing our pages

--- Learning_Nodejs/app.js
+++ Learning_Nodejs/app.js
@@ -1,3 +1,4 @@
+const path= require('path')
 
 const express= require('express');
 const bodyParser= require('body-parser');
@@ -9,11 +10,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 const adminRouter= require ( './routes/admin')
 const shopRouter = require('./routes/shop')
 
-app.use(adminRouter);
+app.use('/admin',adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next )=>{
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 
 
--- Learning_Nodejs/routes/admin.js
+++ Learning_Nodejs/routes/admin.js
@@ -1,15 +1,19 @@
 const express = require('express');
 
+const path =require('path');
+
 const router= express.Router();
 
+const rootDir= require('../utils/path')
+
 router.get('/add-product',(req, res, next)=>{
-    res.send('<form action = "/product"  method ="POST"><input type="text" name= "title"><button type="submit"> Add Product</button> </form');
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
     });
 
 
-router.post('/product' , (req, res, next)=> {
+router.post('/add-product' , (req, res, next)=> {
         console.log(req.body);
         res.redirect('/');
     });
-
+ 
 module.exports= router;
